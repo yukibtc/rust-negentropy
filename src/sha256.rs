c5436278b2793e056d21bf10d66328b69f57739d
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-08-23 13:59:38 +0200

    Allow `clippy::many_single_char_names` in SHA-256 implementation
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- src/sha256.rs
+++ src/sha256.rs
@@ -17,6 +17,7 @@ const SHA256_INIT: [u32; 8] = [
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
 ];
 
+#[allow(clippy::many_single_char_names)]
 pub(crate) fn hash(mut data: Vec<u8>) -> [u8; 32] {
     let original_len: usize = data.len();
     let bit_len: u64 = (original_len * 8) as u64;
